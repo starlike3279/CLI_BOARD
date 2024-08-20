8d9aa91652a3e65f8bd3dc00d246c301274ceb05
taeho5825 <xogh332@naver.com>
2024-08-20 11:52:27 +0900

    7단계

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -49,12 +49,20 @@ public class App {
                 String value = paramsStr[1];
                 int idx = Integer.parseInt(value);
 
+                Article article = null;
                 for(int i = 0; i < articleList.size(); i++){
                     if(articleList.get(i).getId() == idx){
-                        articleList.remove(i);
+                        article = articleList.get(i);
                     }
                 }
-                System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
+
+                if(article == null){
+                    System.out.printf("%d번 게시물은 존재하지 않습니다\n", idx);
+                    continue;
+                }else{
+                    articleList.remove(article);
+                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
+                }
             }
         }
     }
