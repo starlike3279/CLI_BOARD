b79dcd330d44b04516978fe752a2eb0be47af1bd
taeho5825 <xogh332@naver.com>
2024-08-22 10:08:45 +0900

    ãnew cli_board

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,68 +1,36 @@
 package org.example;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import org.example.article.ArticleController;
+import org.example.system.SystemController;
 
 public class App {
-    Scanner sc;
+    ArticleController articleController;
+    SystemController systemController;
 
-    App (Scanner sc) {
-        this.sc = sc;
+    App () {
+        articleController = new ArticleController();
+        systemController = new SystemController();
     }
 
-    void run () {
-        int lastId = 1;
-        List<Article> articleList = new ArrayList<>();
+    public void run () {
         System.out.println("== 게시판 앱 ==");
         while (true) {
             System.out.print("명령) ");
-            String command = sc.nextLine().trim();
+            String command = Container.getSc().nextLine().trim();
+            // 커맨드에 입력한 내용을 actionCode, idx로 분류해서 필드로 저장
+            Request request = new Request(command);
 
-            if (command.equals("종료")) {
+            if (request.getActionCode().equals("종료")) {
+                systemController.exit();
                 break;
-            } else if (command.equals("등록")) {
-                System.out.print("제목 : ");
-                String subject = sc.nextLine().trim();
-                System.out.print("내용 : ");
-                String content = sc.nextLine().trim();
-
-                Article article = new Article(lastId, subject, content);
-                articleList.add(article);
-
-                System.out.printf("%d번 게시물이 등록되었습니다.\n", lastId); // 증감 처리
-                lastId++;
-            } else if (command.equals("목록")) {
-                System.out.println("번호 / 제목 / 내용");
-                System.out.println("----------------------");
-                for (int i = articleList.size() - 1; i > -1; i--) {
-                    Article article = articleList.get(i);
-                    System.out.printf("%d, %s, %s\n", article.getId(), article.getSubject(), article.getContent());
-                }
-            } else if(command.startsWith("삭제")){
-                String[] commandList = command.split("\\?", 2);
-                String actionCode =  commandList[0];
-
-                String[] paramsStr = commandList[1].split("=", 2);
-
-                String key = paramsStr[0];
-                String value = paramsStr[1];
-                int idx = Integer.parseInt(value);
-
-                Article article = null;
-                for(int i = 0; i < articleList.size(); i++){
-                    if(articleList.get(i).getId() == idx){
-                        article = articleList.get(i);
-                    }
-                }
-
-                if(article == null){
-                    System.out.printf("%d번 게시물은 존재하지 않습니다\n", idx);
-                    continue;
-                }else{
-                    articleList.remove(article);
-                    System.out.printf("%d번 게시물이 삭제되었습니다.\n", idx);
-                }
+            } else if (request.getActionCode().equals("등록")) {
+                articleController.write();
+            } else if (request.getActionCode().equals("목록")) {
+                articleController.list();
+            } else if (request.getActionCode().startsWith("삭제")) {
+                articleController.delete(request);
+            } else if (request.getActionCode().startsWith("수정")) {
+                articleController.modify(request);
             }
         }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,9 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        new App(sc).run();
-        sc.close();
+        Container.init();
+        new App().run();
+        Container.close();
     }
 }
